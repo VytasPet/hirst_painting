078662b9b983b5b6a88fafd448e9350b4ae55ff8
Vytautas P <v.petrucionis@gmail.com>
2023-08-08 07:11:09 +0300

    works

---
+++ main.py
@@ -0,0 +1,39 @@
+import colorgram
+import random
+from turtle import Turtle, Screen
+
+t = Turtle()
+colors_list = [(209, 165, 125), (140, 48, 106), (164, 169, 38), (244, 80, 56), (3, 143, 55), (233, 109, 162), (215, 234, 232), (241, 65, 140)]
+
+def rgb_to_hex(rgb):
+    return "#{:02x}{:02x}{:02x}".format(*rgb)
+
+def panting():
+    pos = 0
+    while pos < 210:
+        long = 0
+        t.setposition(0, pos)
+        while long < 210:
+            t.pendown()
+            t.dot(20, rgb_to_hex(random.choice(colors_list)))
+            t.penup()
+            t.forward(40)
+            long += 40
+        pos += 40
+
+panting()
+
+
+colors = colorgram.extract('hir.jpg', 10)
+
+cols =[]
+
+for c in colors:
+    r = c.rgb.r
+    g = c.rgb.g
+    b = c.rgb.b
+    new_col = (r, g, b)
+    cols.append(new_col)
+
+screen = Screen()
+screen.exitonclick()
